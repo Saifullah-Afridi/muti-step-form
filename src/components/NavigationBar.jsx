9fe2bf5cf21aa2189cc4b1c6d680f89e165a23a6
Saifullah-Afridi <saifullahafridi08@gmail.com>
2024-10-18 17:50:55 +0500

    done

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { FaMagnifyingGlass } from "react-icons/fa6";
-import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  FaClosedCaptioning,
+  FaCross,
+  FaMagnifyingGlass,
+  FaX,
+} from "react-icons/fa6";
+import { GiCrossMark, GiHamburgerMenu } from "react-icons/gi";
+import { FaCut } from "react-icons/fa";
 const NavigationBar = () => {
+  const [openHam, setOpenHam] = useState();
+
+  const handleOpenHamburgur = () => {
+    setOpenHam(true);
+    console.log("hello");
+  };
+
+  const handleCloseHamburger = () => {
+    setOpenHam(false);
+  };
   return (
     <header>
       <nav className="h-[60px] bg-green-300">
@@ -12,20 +28,50 @@ const NavigationBar = () => {
             <h2 className="text-3xl font-bold">PDMS</h2>
           </div>
           <div className="hidden md:flex justify-between gap-3 ">
-            <NavLink to="#" className="p-2 text-lg">
+            <NavLink to="#" className="p-2 text-lg hover:bg-green-500 ">
               Home
             </NavLink>
-            <NavLink to="#" className="p-2 text-lg">
+            <NavLink to="#" className="p-2 text-lg  hover:bg-green-500 ">
               About Us
             </NavLink>
-            <NavLink to="#" className="p-2 text-lg  ">
+            <NavLink to="#" className="p-2 text-lg    hover:bg-green-500">
               Contact Us
             </NavLink>
           </div>
           <div className="md:hidden p-2 ">
-            <button className="text-3xl">
+            <button className="text-3xl" onClick={handleOpenHamburgur}>
               <GiHamburgerMenu />
             </button>
+            <div
+              className={`fixed top-0 right-0 h-full w-1/3 bg-green-400 shadow-lg transform transition-transform  duration-300 ease-in-out ${
+                openHam ? "translate-x-0" : "translate-x-full"
+              } `}
+            >
+              <button
+                className="ml-2 bg-red-200 p-1 "
+                onClick={handleCloseHamburger}
+                type="button"
+              >
+                <FaX />
+              </button>
+              <div>
+                <div className=" flex justify-center gap-1 items-center">
+                  <h2 className="text-2xl font-bold ">Logo</h2>
+                  <h2 className="text-2xl font-bold">PDMS</h2>
+                </div>
+                <div className="flex flex-col gap-1  ">
+                  <NavLink to="#" className="p-1 text-lg  hover:bg-green-600">
+                    Home
+                  </NavLink>
+                  <NavLink to="#" className="p-1 text-lg  hover:bg-green-600">
+                    About Us
+                  </NavLink>
+                  <NavLink to="#" className="p-1 text-lg    hover:bg-green-600">
+                    Contact Us
+                  </NavLink>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="hidden md:block">
             <form>
